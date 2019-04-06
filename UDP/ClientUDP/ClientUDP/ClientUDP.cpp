2b1ba2dc2492083010069becc22c0315ab9d206d
Rapzy <andrushko.egor@gmail.com>
2019-04-06 21:07:28 +0300

    Add UDP server + client

---
+++ UDP/ClientUDP/ClientUDP/ClientUDP.cpp
@@ -0,0 +1,53 @@
+﻿#include <conio.h>
+#include <iostream>
+#include <io.h>
+#include <winsock2.h>
+#include <Ws2tcpip.h>
+#include <stdio.h>
+
+// Link with ws2_32.lib
+#pragma comment(lib, "Ws2_32.lib")
+using namespace std;
+
+int main()
+{
+	int rc, size, symbols, y = 0, i = 1; 
+	WSADATA wsd;
+	WSAStartup(MAKEWORD(2,2), &wsd);
+	SOCKET s = socket(AF_INET, SOCK_DGRAM, 0);
+
+	sockaddr_in serveraddr;
+	serveraddr.sin_family = AF_INET;
+	serveraddr.sin_port = htons(27015);
+	serveraddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+
+	FILE *f;
+	f = fopen("../../../send.txt", "rb");
+
+	while (!feof(f))
+	{
+		char bufer[100];
+		symbols = fread(bufer, 1, 100, f);
+		size = ftell(f);
+		printf("read symbols: %d, part: %d, pos: %ld \n", symbols, i, size);
+		printf("\n");
+		printf("%s", bufer);
+		int iResult = sendto(s, bufer, symbols, 0, (sockaddr*)&serveraddr, sizeof(serveraddr));
+		if (iResult == SOCKET_ERROR) {
+			wprintf(L"sendto failed with error: %d\n", WSAGetLastError());
+			closesocket(s);
+			WSACleanup();
+			return 1;
+		}
+		printf("%d OK", iResult);
+		i++;
+	}
+	char buf[4] = "EOF";
+	sendto(s, buf, sizeof(buf), 0, (sockaddr*)&serveraddr, sizeof(serveraddr));
+	fclose(f);
+	_getch();
+	closesocket(s);
+	WSACleanup();
+	return 0;
+}
---
+++ UDP/ServerUDP/ServerUDP/ServerUDP.cpp
@@ -0,0 +1,56 @@
+﻿#include <conio.h>
+#include <iostream>
+#include <io.h>
+#include <winsock2.h>
+#include <Ws2tcpip.h>
+#include <stdio.h>
+
+// Link with ws2_32.lib
+#pragma comment(lib, "Ws2_32.lib")
+using namespace std;
+
+int main()
+{
+	int rc, i = 1;
+	WSADATA wsd;
+	WSAStartup(MAKEWORD(2, 2), &wsd);
+	FILE *f;
+
+	SOCKET s = socket(AF_INET, SOCK_DGRAM, 0);
+
+	struct sockaddr_in serveraddr, clientaddr;
+	memset(&serveraddr, 0, sizeof(serveraddr));
+	memset(&clientaddr, 0, sizeof(clientaddr));
+
+	serveraddr.sin_family = AF_INET;
+	serveraddr.sin_port = htons(27015);
+	serveraddr.sin_addr.s_addr = INADDR_ANY;
+
+	rc = bind(s, (struct sockaddr*)&serveraddr, sizeof(serveraddr));
+	if (rc < 0) printf("error");
+	listen(s, 5);
+	int clientsize = sizeof(clientaddr);
+	f = fopen("../../../recieve.txt", "ab");
+	while (true)
+	{
+		puts("Waiting");
+		char buf[100];
+		int r = recvfrom(s, buf, sizeof(buf), 0, (sockaddr *)&clientaddr, &clientsize);
+		if (r > 0)
+		{
+			if (strcmp(buf,"EOF")) {
+				cout << "receive bytes: " << r << ", part: " << i << endl;
+				printf("%s\n", buf);
+				i++;
+			}
+			else {
+				break;
+			}
+		}
+	}
+	fclose(f);
+	_getch();
+	closesocket(s);
+	WSACleanup();
+	return 0;
+}
